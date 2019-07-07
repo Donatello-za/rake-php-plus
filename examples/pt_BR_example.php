170a8779e2803ff3b2e50e56bb6f0f309db4b09b
Igor Carvalho <igor.carvalho@rakuten.com>
2019-07-08 00:51:21 +0200

    Implemented stopwords for brazilian portuguese and an example

---
+++ examples/pt_BR_example.php
@@ -0,0 +1,22 @@
+<?php
+
+// Para ejecutar este ejemplo desde la línea de comando.:
+// php ./examples/es_AR_example.php "Algun texto de ejemplo"
+
+require 'vendor/autoload.php';
+
+use DonatelloZa\RakePlus\RakePlus;
+
+if ($argc < 2) {
+    echo "Especifique el texto que desea analizar, por ejemplo:\n";
+    echo "php ./examples/es_AR_example.php \"Algún texto de ejemplo del que me gustaría extraer palabras clave.\"\n";
+    exit(1);
+}
+
+$keywords = RakePlus::create($argv[1], 'pt_BR')->keywords();
+print "Resultados de palabras clave: {$argv[1]}\n";
+print_r($keywords);
+
+$phrases = RakePlus::create($argv[1], 'pt_BR')->scores();
+print "Resultados de la frase: {$argv[1]}\n";
+print_r($phrases);
