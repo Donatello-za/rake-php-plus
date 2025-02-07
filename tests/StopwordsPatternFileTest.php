1de37c131b1b2966031897f205bf10e4b21d2144
Sergey Kudashev <kudashevs@gmail.com>
2025-02-07 19:34:16 +0300

    Refactor StopwordsPatternFile remove a redundant else in the loadLangPHPFile method

---
+++ tests/StopwordsPatternFileTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use DonatelloZa\RakePlus\StopwordsPatternFile;
+use PHPUnit\Framework\TestCase;
+
+class StopwordsPatternFileTest extends TestCase
+{
+    public function testThrowsExceptionWhenNonExistingFile()
+    {
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('file');
+
+        StopwordsPatternFile::create('wrong');
+    }
+}
